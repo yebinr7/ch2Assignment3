5a552e66c917ea24b9e96322fb02f051fe76bd08
yebinr7 <yebinr7@gmail.com>
2025-01-02 20:18:22 +0900

    도전과제 완료

--- ch2Assignment3/SimpleVector.h
+++ ch2Assignment3/SimpleVector.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <algorithm>//sort이용하기 위해서 어쩔수 없이 인클루드
+#include <iostream>//cout 하려고..
 
 //타입 의존 x 템플릿 이용
 //생성자, 기본 생성자 크기10인 배열
@@ -17,12 +19,30 @@ public:
 	SimpleVector(int n);
 	~SimpleVector();
 
+	//복사 생성자
+	//SimpleVector(const T& other);//->모호함 int 들어오면 위 생성자랑같다.
+	SimpleVector(const SimpleVector<T>& other);
+
+
+
+	////복사 연산자
+	//T& operator=(const T& other);
+
+	////무브 생성자
+	//SimpleVector(T&& other/*소유권뺏어야하니깐*/);
+
+	////무브 연산자 
+	//T& operator=(T&& other);
+
 
 public:
 	void push_back(const T& var);
 	void pop_back();
 	int size();
 	int capacity();
+	void resize(int newCapacity);
+	void sortData();//sort이용
+	void printVector();
 
 private:
 
@@ -46,11 +66,11 @@ inline SimpleVector<T>::SimpleVector()
 	//기본형 capacity 10으로 만들기
 	//_data = new T[10];
 	//0으로 모두 초기화
-	int c = capacity();
+	/*int c = capacity();
 	for (size_t i = 0; i < c ; i++)
 	{
 		_data[i] = 0;
-	}
+	}*/
 
 };
 
@@ -60,25 +80,87 @@ inline SimpleVector<T>::SimpleVector(int n)
 	_currentSize(0),
 	_currentCapacity(n)
 {
-	int c = capacity();
-	for (size_t i = 0; i < c; i++)
-	{
-		_data[i] = 0;
-	}
+	//int c = capacity();
+	//for (size_t i = 0; i < c; i++)
+	//{
+	//	_data[i] = 0;
+	//}
 };
 
-
+//소멸자
 template<typename T>
 inline SimpleVector<T>::~SimpleVector()
 {
 	delete[]_data; //배열은 항상 삭제이렇게 
-};
+}
+
+//복사생성자
+template<typename T>
+inline SimpleVector<T>::SimpleVector(const SimpleVector<T>& other)
+	:_currentSize(other._currentSize),
+	 _currentCapacity(other._currentCapacity)
+{
+	//포인터는 조심
+	_data = new T[_currentCapacity];
+	//내용물 복사해오기
+
+	for (size_t i = 0; i <_currentSize ; i++)
+	{
+		_data[i] = other._data[i];
+	}
+}
+
+//////복사 생성자
+//template<typename T>
+//inline SimpleVector<T>::SimpleVector(const T& other)
+//{
+//
+//}
+
+
+////복사 연산자
+//template<typename T>
+//inline T& SimpleVector<T>::operator=(const T& other)
+//{
+//	// TODO: 여기에 return 문을 삽입합니다.
+//}
+//
+////이동 생성자
+//template<typename T>
+//inline SimpleVector<T>::SimpleVector(T&& other)
+//{
+//}
+//
+//
+////이동 연산자
+//template<typename T>
+//inline T& SimpleVector<T>::operator=(T&& other)
+//{
+//	// TODO: 여기에 return 문을 삽입합니다.
+//}
+
+
+
 
 template<typename T>
 inline void SimpleVector<T>::push_back(const T& var)
 {
 	if (_currentSize >= _currentCapacity)
-		return;//아무일 일어나지 않는다.
+	{
+		//배열이 가득 찼을경우
+		//기존 배열보다 5만큼 늘리고 거기다 추가
+		//힙 배열 새로 만들기
+		_currentCapacity += 5;//5만큼 늘리기
+		T* newArray = new T[_currentCapacity];
+
+		for (size_t i = 0; i < _currentSize; i++)
+		{
+			newArray[i] = _data[i]; //내용물 복사
+		}
+		delete[] _data;
+		_data = nullptr;
+		_data = newArray;//주소 가져가기
+	}
 
 	_data[_currentSize] = var;
 	_currentSize++;
@@ -101,4 +183,53 @@ inline int SimpleVector<T>::capacity()
 {
 	return _currentCapacity;
 }
-;
+
+template<typename T>
+inline void SimpleVector<T>::resize(int newCapacity)
+{
+	//정수 하나 받고받은 정수가 지금 배열의 capacity보다 작으면 return;
+	if (newCapacity <= _currentCapacity)
+		return;
+	else
+	{
+		//크면 재할당 & 복사 
+		
+		T* newArray = new T[newCapacity];
+
+		for (size_t i = 0; i < _currentSize; i++)
+		{
+			newArray[i] = _data[i]; //내용물 복사
+		}
+		delete[] _data;
+		_data = nullptr;
+		_data = newArray;//주소 가져가기
+		_currentCapacity = newCapacity; //새로운capa로 업뎃
+	}
+}
+template<typename T>
+inline void SimpleVector<T>::sortData()
+{
+	std::sort(_data, _data + _currentSize); //std 포인터에도 쓸수있구나..
+}
+
+template<typename T>
+inline void SimpleVector<T>::printVector()
+{
+	std::cout << "현재 벡터 사이즈: " << _currentSize << ", ";
+	std::cout << "현재 벡터 용량: " << _currentCapacity << "\n";
+	std::cout << "현재 배열 요소:";
+	for (size_t i = 0; i < _currentSize; i++)
+	{
+		if (i >= _currentSize-1)
+		{
+			std::cout << _data[i] << std::endl;
+		}
+		else
+		{
+			std::cout << _data[i] << ", ";
+		}
+	}
+	//size가 0인경우
+	if (_currentSize == 0)
+		std::cout << "사이즈가 0입니다.\n";
+}
--- ch2Assignment3/main.cpp
+++ ch2Assignment3/main.cpp
@@ -8,18 +8,32 @@ int main(void)
 {
 	SimpleVector<int> sv1;
 	SimpleVector<int> sv2(4);
-
-	sv1.push_back(1);
-	sv1.push_back(2);
-	sv1.push_back(3);
-	sv1.push_back(4);
-	sv1.push_back(5);
-	sv1.push_back(6);
-	sv1.push_back(7);
-	sv1.push_back(8);
-	sv1.push_back(9);
-	sv1.push_back(10);
+	sv2.printVector();
+	
 	sv1.push_back(11);//<-초과일때 if문 되는지체크
+	sv1.push_back(10);
+	sv1.push_back(9);
+	sv1.push_back(8);
+	sv1.push_back(7);
+	sv1.push_back(6);
+	sv1.push_back(5);
+	sv1.push_back(4);
+	sv1.push_back(3);
+	sv1.push_back(2);
+	sv1.push_back(1);
+	
+	sv1.sortData();//오름차순 정렬 
+	sv1.printVector();
+
+	SimpleVector<int> sv3(sv1);//복사 생성자 실행
+	sv3.printVector();
+	sv3.push_back(12);//사이즈초과시 재할당&복사(이사)
+	sv3.printVector();
+
+
+	sv3.resize(1000);//사이즈 재할당 & 복사(이사)
+	sv3.printVector();
+
 
 	sv1.pop_back();
 	sv1.pop_back();
@@ -32,7 +46,13 @@ int main(void)
 	sv1.pop_back();
 	sv1.pop_back();
 	sv1.pop_back();//<-초과일때 if문 되는지체크
+	sv1.printVector();
 
+	for (size_t i = 0; i < 12; i++)
+	{
+		sv3.pop_back();
+	}
+	sv3.printVector();
 
 	return 0;
 }
